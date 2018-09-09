c27658201cee90d5141fdc8b8ba09e1455be6d80
daniel carlos <danielcarlossr1@gmail.com>
2018-09-09 08:14:47 -0300

    commit sistema semipronto

---
+++ client/src/components/Index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <!-- pagina teste -->
+    <h1>Consultar</h1>
+    
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+    }
+  },
+  beforeMount () {
+      const resposta = AuthenticationService.consultar()
+      
+      console.log(resposta);
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ client/src/components/Register.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <!-- pagina teste -->
+    <h1>Register</h1>
+    <input type="text" name="nome" v-model="nome" placeholder="Nome">
+    <br>
+    <input type="text" name="img" v-model="img" placeholder="URL da imagem">
+    <br>
+    <button @click="register">Cadastrar</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      nome: '',
+      img: ''
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+          nome: this.nome,
+          img: this.img
+      })
+      console.log(response.data); 
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
